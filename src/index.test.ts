17be0dd3270ebe04843ced8bb5108c05a5839162
Miguel Oller <maoa22@gmail.com>
2020-03-08 22:13:56 -0400

    Add tests for interpolateShellCommands

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,4 +1,4 @@
-import { matchInterpolatedShellCommand } from ".";
+import { matchInterpolatedShellCommand, interpolateShellCommands } from ".";
 
 describe("dotenv-command", () => {
   describe("matchInterpolatedShellCommand", () => {
@@ -50,6 +50,36 @@ describe("dotenv-command", () => {
       matches.forEach(match => {
         expect(match).toBeNull();
       });
+    });
+  });
+
+  describe("interpolateShellCommands", () => {
+    it("interpolates a basic shell command", () => {
+      let actual = interpolateShellCommands("$(echo foo)");
+      let expected = "foo";
+
+      expect(actual).toBe(expected);
+    });
+
+    it("interpolates multiple basic shell commands", () => {
+      let actual = interpolateShellCommands("$(echo foo) $(echo bar)");
+      let expected = "foo bar";
+
+      expect(actual).toBe(expected);
+    });
+
+    it("interpolates a nested shell command", () => {
+      let actual = interpolateShellCommands("$(echo $(echo foo))");
+      let expected = "foo";
+
+      expect(actual).toBe(expected);
+    });
+
+    it("does not interpolate an escaped shell command", () => {
+      let actual = interpolateShellCommands("\\$(echo foo)");
+      let expected = "\\$(echo foo)";
+
+      expect(actual).toBe(expected);
     });
   });
 });
